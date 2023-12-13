2dfdf6f18c3f6cabaf29ba3dba32e3a600ae0ac0
LeeNakKyu <nakku1055@naver.com>
2023-12-13 18:09:16 +0900

    move: [petCRUD] petCRUD 코드 옮김
    - index.js -> pets.router.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { reviewsRouter } from './reviews.router.js';
 import { usersRouter } from './users.router.js';
+import { petsRouter } from './pets.router.js';
 
 const apiRouter = Router();
 
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/reviews', reviewsRouter);
-
+apiRouter.use('/pets', petsRouter);
 
 export { apiRouter };
