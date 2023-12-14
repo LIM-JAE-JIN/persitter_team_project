3cb8c00f26677d01097a9002d2942ef14cb56c40
JAEJIN <wowls9416@naver.com>
2023-12-14 17:42:40 +0900

    feat: [front_main] 헤더, 푸터, 메인 작업

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,3 +13,26 @@ function drPopupClose(im) {
     $(im).closest('.dr-popup-wrap').css('display', 'none');
     $('.dr-dim').css('display', 'none');
 }
+
+$(document).ready(function () {
+    // 세션 ID를 가져옵니다.
+    var sessionId = getCookie('connect.sid');
+
+    // 세션 ID가 있는지 여부에 따라 탭을 토글합니다.
+    if (sessionId) {
+        // 세션 ID가 있으면 로그인 상태로 간주하고 로그인 탭을 표시합니다.
+        $('#loginTab').hide();
+        $('#logoutTab').show();
+    } else {
+        // 세션 ID가 없으면 로그아웃 상태로 간주하고 로그아웃 탭을 표시합니다.
+        $('#loginTab').show();
+        $('#logoutTab').hide();
+    }
+});
+
+// 쿠키에서 특정 이름의 쿠키 값을 가져오는 함수
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+}
