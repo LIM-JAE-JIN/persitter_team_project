c74b6a267ef59373da63f2e3ab37c83a929efba8
Han9526 <jk2tmdwns@naver.com>
2023-12-14 10:22:55 +0900

    feat: [appointment] 예약 조회,생성,삭제 기능구현

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { reviewsRouter } from './reviews.router.js';
 import { usersRouter } from './users.router.js';
-
+import { appointmentRouter } from './appointment.router.js';
 const apiRouter = Router();
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/reviews', reviewsRouter);
-
+apiRouter.use('/appointments', appointmentRouter);
 export { apiRouter };
